3902ac8e263b39f3ace950bc71902cde4d98ce6b
BuildTools <unconfigured@null.spigotmc.org>
2021-09-20 18:14:38 -0400

    ics homework grade 11 review 0.1 and 0.2

---
+++ 11 review pt2/questionTwo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class questionTwo {
+    
+    public static void main (String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        int num = Integer.parseInt(input.nextLine());
+        int res = 2;
+        int count = 1;
+
+        while(res <= num)
+        {
+            res = (int)Math.pow(2, count);
+            count++;
+        }
+        System.out.println(res + " is the smallest power of 2 greater than " + num);
+
+        input.close();
+    }
+
+}
