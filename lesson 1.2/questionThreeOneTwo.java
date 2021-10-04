90482a488af63368d3c327735e93bf93a8245ae3
Hai Shi <58992834+Biggeroof@users.noreply.github.com>
2021-10-04 18:59:16 -0400

    Update questionThreeOneTwo.java

--- lesson 1.2/questionThreeOneTwo.java
+++ lesson 1.2/questionThreeOneTwo.java
@@ -100,7 +100,7 @@ public class questionThreeOneTwo
         System.out.println();
 
         System.out.println("Rotating the array by 90 degrees clockwise:");
-        int rotated[][] = rotate(arr);
+        int[][] rotated = rotate(arr);
         printArray(rotated);
     }
     
