8f49f44ee7891e5ec5c9646b02cfe2f74258a77b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-04 21:35:41 -0400

    updated the code

--- 11 review pt4/questionThreeFour.java
+++ 11 review pt4/questionThreeFour.java
@@ -1,36 +1,31 @@
 import java.util.Scanner;
 
 public class questionThreeFour {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
         System.out.print("Input: ");
         String numbers = input.nextLine();
         int total = 0;
         int add;
         int len = numbers.length();
-
-        if(numbers.length() % 2 == 0)
-        {
-            for(int i = 2; i <= len; i += 2 )
-            {
-                add = Integer.parseInt(numbers.substring(i - 2, i));
-                total += add;
-            }
-        }
-        else
-        {
-            for(int i = 2; i <= len - 1; i += 2 )
-            {
-                add = Integer.parseInt(numbers.substring(i - 2, i));
-                total += add;
-            }
-            
-            add = Integer.parseInt(numbers.substring(len - 1));
-            total += add;
-        }
-        System.out.print("Output: ");
-        System.out.println(total);
-
-    }
+		
+		if(numbers.length() % 2 == 0)
+		{
+		    for(int i = 2; i <= numbers.length(); i += 2)
+		    {
+		        total += Integer.parseInt(numbers.substring(i - 2, i));
+		    }
+		}
+		
+		else
+		{
+		    for(int i = 2; i <= (numbers.length() - 1); i += 2)
+		    {
+		        total += Integer.parseInt(numbers.substring(i - 2, i));
+		    }
+		    total += Integer.parseInt(numbers.substring(numbers.length() - 1));
+		}
+		System.out.println(total);
+	}
 }
