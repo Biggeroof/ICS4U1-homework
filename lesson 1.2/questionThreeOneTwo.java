e4d9ca0fecedd299e0903313668368f44e3ef3c4
BuildTools <unconfigured@null.spigotmc.org>
2021-10-06 22:15:28 -0400

    added some comments

--- lesson 1.2/questionThreeOneTwo.java
+++ lesson 1.2/questionThreeOneTwo.java
@@ -2,29 +2,41 @@ public class questionThreeOneTwo
 {
     public static void printArray(int[][] arr)
     {
+        //make length row length - 1 since array indexes start at 0
         int lenr = arr.length - 1;
         int lenc;
+        //print out the inital bracket
         System.out.print("[ ");
+        //iterate through the rows except for the last one
         for(int i = 0; i < lenr; i++)
         {
+            //print bracket for the inside of the 2d array
             System.out.print("[");
+            //set lenc to 1 less than the length of the array at that row (array indexes start at 0)
             lenc = arr[i].length - 1;
+            //printing all the column indexes in that row
             for(int j = 0; j < lenc; j++)
             {
+                //print out [row][column] followed by a comma
                 System.out.print(arr[i][j] + ", ");
             }
+            //print out the last element in that row
             System.out.print(arr[i][lenc]);
             System.out.println("],");
         }
 
+        //set lenc to the length of the last row
         lenc = arr[lenr].length - 1;
 
         System.out.print("[");
         for(int j = 0; j < lenc; j++)
-            {
-                System.out.print(arr[lenr][j] + ", ");
-            }
+        {
+            //print out last row excpet for the last element 
+            System.out.print(arr[lenr][j] + ", ");
+        }
+        //print out the last column element in the row
         System.out.print(arr[lenr][lenc]);
+        //print the ending brackets
         System.out.print("] ]");
         System.out.println();
     }
@@ -33,6 +45,7 @@ public class questionThreeOneTwo
     {
         for(int i = 0; i < arr[0].length; i++)
         {
+            //iterate down the columns and swap row elements
             int temp = arr[row1][i];
             arr[row1][i] = arr[row2][i];
             arr[row2][i] = temp;
@@ -44,6 +57,7 @@ public class questionThreeOneTwo
         //col1 = 0 col2 = 1
         for(int i = 0; i < arr.length; i++)
         {
+            //iterate down rows and swap column elements
             int temp = arr[i][col1];
             arr[i][col1] = arr[i][col2];
             arr[i][col2] = temp;
@@ -52,6 +66,7 @@ public class questionThreeOneTwo
 
     public static void multiplyRow(int[][] arr, int row, int num) 
     {
+        //iterate down row and multiply by num
         for(int i = 0; i < arr[0].length; i++)
         {
             arr[row][i] *= num;
@@ -60,18 +75,18 @@ public class questionThreeOneTwo
 
     public static int[][] rotate(int[][] arr) 
     {
-        //length variables for the rotated array
-        int lenr = arr[0].length;   
+        //length variables for the rotated array (swapped with the original array)
+        int lenr = arr[0].length;
         int lenc = arr.length;
         int[][] rotated = new int[lenr][lenc];
 
-        //row for new
+        //row for new array
         for(int i = 0; i < lenr; i++)
         {
-            //column for new
+            //column for new array
             for(int j = 0; j < lenc; j++)
             {
-                //swap the rows and columns
+                //swap the rows and columns (start from back of original column)
                 rotated[i][lenc - (j + 1)] = arr[j][i];
             }
         }
@@ -89,23 +104,23 @@ public class questionThreeOneTwo
 
         System.out.println();
 
-//         System.out.println("Swapping rows 1 and 2:");
-//         swapRow(arr, 0, 1);
-//         printArray(arr);
+        // System.out.println("Swapping rows 1 and 2:");
+        // swapRow(arr, 0, 1);
+        // printArray(arr);
 
-//         System.out.println();
+        // System.out.println();
 
-//         System.out.println("Swapping columns 1 and 2:");
-//         swapColumn(arr, 0, 1);
-//         printArray(arr);
+        // System.out.println("Swapping columns 1 and 2:");
+        // swapColumn(arr, 0, 1);
+        // printArray(arr);
 
-//         System.out.println();
+        // System.out.println();
 
-//         System.out.println("Multiplying row 2 by 2:");
-//         multiplyRow(arr, 1, 2);
-//         printArray(arr);
+        // System.out.println("Multiplying row 2 by 2:");
+        // multiplyRow(arr, 1, 2);
+        // printArray(arr);
 
-//         System.out.println();
+        // System.out.println();
 
         System.out.println("Rotating the array by 90 degrees clockwise:");
         int[][] rotated = rotate(arr);
--- lesson 1.2/questionTwoOneTwo.java
+++ lesson 1.2/questionTwoOneTwo.java
@@ -5,6 +5,7 @@ public class questionTwoOneTwo
 
     public static int arraySum(int row, int[][] arr)
     {
+        //iterate down entire row and add
         int total = 0;
         for (int i = 0; i < arr[row - 1].length; i++)
         {
@@ -15,29 +16,41 @@ public class questionTwoOneTwo
 
     public static void printArray(int[][] arr)
     {
+        //make length row length - 1 since array indexes start at 0
         int lenr = arr.length - 1;
         int lenc;
+        //print out the inital bracket
         System.out.print("[ ");
+        //iterate through the rows except for the last one
         for(int i = 0; i < lenr; i++)
         {
+            //print bracket for the inside of the 2d array
             System.out.print("[");
+            //set lenc to 1 less than the length of the array at that row (array indexes start at 0)
             lenc = arr[i].length - 1;
+            //printing all the column indexes in that row
             for(int j = 0; j < lenc; j++)
             {
+                //print out [row][column] followed by a comma
                 System.out.print(arr[i][j] + ", ");
             }
+            //print out the last element in that row
             System.out.print(arr[i][lenc]);
             System.out.println("],");
         }
 
+        //set lenc to the length of the last row
         lenc = arr[lenr].length - 1;
 
         System.out.print("[");
         for(int j = 0; j < lenc; j++)
-            {
-                System.out.print(arr[lenr][j] + ", ");
-            }
+        {
+            //print out last row excpet for the last element 
+            System.out.print(arr[lenr][j] + ", ");
+        }
+        //print out the last column element in the row
         System.out.print(arr[lenr][lenc]);
+        //print the ending brackets
         System.out.print("] ]");
         System.out.println();
     }
