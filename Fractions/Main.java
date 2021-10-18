c275a5e2a8389e0aafbb9c277a72218f4462d035
BuildTools <unconfigured@null.spigotmc.org>
2021-10-18 17:36:21 -0400

    ics homework for objects lesson on oct 18 2021

--- Fractions/Main.java
+++ Fractions/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Fraction f = new Fraction(3, 1);
+        Fraction f = new Fraction(3, 5);
         System.out.println("f: " + f);
 
-        Fraction g = new Fraction(-8.343);
+        Fraction g = new Fraction(7, 10);
         System.out.println("g: " + g);
 
         System.out.println("is f greater than g?: " + f.greaterThan(g));
@@ -11,19 +11,39 @@ public class Main {
         Fraction multiply = product(f, g);
         System.out.println("Product of " + f + " and " + g + ": " + multiply);
 
-        //Fraction add = f.add(g);
-        //System.out.println(add);
+        //System.out.println("Adding " + f + " and " + g + ": " + f.add(g));
+        System.out.println("Adding " + f + " and " + g + ": " + sum(f, g));
 
-        //Fraction test = new Fraction(3, 6);
-        //System.out.println(test.gcd(test.getNumerator(), test.getDenominator()));
+        System.out.println("Subtracting " + f + " by " + g + ": " + difference(f, g));
 
-        System.out.println("are they equal?: " + f.equals(g));
+        //System.out.println("Dividing " + f + " by " + g + ": " + f.divide(g));
+        System.out.println("Dividing " + f + " by " + g + ": " + quotient(f, g));
 
+        System.out.println("Reciprocal of " + f + ": " + f.reciprocal());
+
+        System.out.println("Are they equal?: " + f.equals(g));
 
     }
 
-    public static Fraction product(Fraction f1, Fraction f2) {
+    public static Fraction product(Fraction f1, Fraction f2) 
+    {
         return f1.multiply(f2);
+    }
+
+    public static Fraction sum(Fraction f1, Fraction f2) 
+    {
+        return f1.add(f2);
+    }
+
+    public static Fraction difference(Fraction f1, Fraction f2) 
+    {
+        f2.setNumerator(f2.getNumerator() * -1);
+        return f1.add(f2);
+    }
+
+    public static Fraction quotient(Fraction f1, Fraction f2) 
+    {
+        return f1.divide(f2);
     }
 
 }
