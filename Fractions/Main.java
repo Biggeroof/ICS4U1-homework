98d3acc1da06659d76334413a2afd899db79cfa8
Hai Shi <58992834+Biggeroof@users.noreply.github.com>
2021-10-19 13:11:47 -0400

    Update Main.java

--- Fractions/Main.java
+++ Fractions/Main.java
@@ -8,30 +8,25 @@ public class Main {
 
         System.out.println("is f greater than g?: " + f.greaterThan(g));
         //Fraction multiply = f.multiply(g);
-        Fraction multiply = product(f, g);
-        System.out.println("Product of " + f + " and " + g + ": " + multiply);
+        System.out.println("Product of " + f + " and " + g + ": " + Fraction.product(f, g));
 
-        System.out.println("g: " + g);
 
         //System.out.println("Adding " + f + " and " + g + ": " + f.add(g));
-        System.out.println("Adding " + f + " and " + g + ": " + sum(f, g));
+        System.out.println("Adding " + f + " and " + g + ": " + Fraction.sum(f, g));
 
-        System.out.println("g: " + g);
 
-        System.out.println("Subtracting " + f + " by " + g + ": " + difference(f, g));
+        System.out.println("Subtracting " + f + " by " + g + ": " + Fraction.difference(f, g));
 
-        System.out.println("g: " + g);
 
         //System.out.println("Dividing " + f + " by " + g + ": " + f.divide(g));
-        System.out.println("Dividing " + f + " by " + g + ": " + quotient(f, g));
+        System.out.println("Dividing " + f + " by " + g + ": " + Fraction.quotient(f, g));
 
-        System.out.println("g: " + g);
 
         System.out.println("Reciprocal of " + f + ": " + f.reciprocal());
 
         System.out.println("Are they equal?: " + f.equals(g));
-        
-        //all of these change the original f object directly 
+
+        //all of these change the original f object directly
         System.out.println("multiplying f by g: ");
         f.timesEquals(g);
         System.out.println("f: " + f);
@@ -44,29 +39,5 @@ public class Main {
         f.integerMultiply(3);
         System.out.println("f: " + f);
 
-    }
-
-    public static Fraction product(Fraction f1, Fraction f2)
-    {
-        return f1.multiply(f2);
-    }
-
-    public static Fraction sum(Fraction f1, Fraction f2)
-    {
-        return f1.add(f2);
-    }
-
-    public static Fraction difference(Fraction f1, Fraction f2)
-    {
-        Fraction subtract = new Fraction(f2.getNumerator(), f2.getDenominator());
-        //Fraction subtract = f2;
-        //this is a reference to f2, so it mutates f2 (won't work)
-        subtract.setNumerator(subtract.getNumerator() * -1);
-        return f1.add(subtract);
-    }
-
-    public static Fraction quotient(Fraction f1, Fraction f2)
-    {
-        return f1.divide(f2);
     }
 }
