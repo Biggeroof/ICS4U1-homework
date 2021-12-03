e31f7b692a9e9fb585d22a22f4d4fdcc34866d32
Hai Shi <58992834+Biggeroof@users.noreply.github.com>
2021-12-03 13:43:30 -0500

    Update PartTimeEmployee.java

--- ShiHaiPayroll/src/payroll/PartTimeEmployee.java
+++ ShiHaiPayroll/src/payroll/PartTimeEmployee.java
@@ -7,6 +7,17 @@ public class PartTimeEmployee extends Employee
     private double hourlyWage;
     private double sickDaysTaken;
 
+    /**
+     * Initializes a PartTimeEmployee object instance and fills in all the fields with the parameters
+     *
+     * @param employeeNumber The employee's id
+     * @param lastName The employee's last name
+     * @param firstName The employee's first name
+     * @param jobTitle The employee's job
+     * @param numHoursAssigned
+     * @param hourlyWage
+     * @param sickDaysTaken
+     */
     public PartTimeEmployee (String employeeNumber, String lastName, String firstName, String jobTitle, double numHoursAssigned, double hourlyWage, double sickDaysTaken)
     {
         super(employeeNumber, lastName, firstName, jobTitle);
@@ -15,36 +26,68 @@ public class PartTimeEmployee extends Employee
         this.sickDaysTaken = sickDaysTaken;
     }
 
+    /**
+     * Returns the amount of hours the employee works per month
+     *
+     * @return The number of hours the employee is assigned
+     */
     public double getNumHoursAssigned()
     {
         return numHoursAssigned;
     }
 
+    /**
+     * Returns the hourly wage of the employee
+     *
+     * @return THe hourly wage
+     */
     public double getHourlyWage()
     {
         return hourlyWage;
     }
 
+    /**
+     * Calculates the amount of money that the employee will make for the current month
+     *
+     * @return The amount of money made in that month
+     */
     public double pay()
     {
         return hourlyWage * (numHoursAssigned - (sickDaysTaken * HOURS_PER_DAY));
     }
 
+    /**
+     * Uses a certain amount of sick days for the employee
+     *
+     * @param amount The amount of sick days to be used
+     */
     public void useSickDay(double amount)
     {
         sickDaysTaken += amount;
     }
 
+    /**
+     * Resets the amount of sick days used by the employee to 0
+     */
     public void resetSickDays()
     {
         sickDaysTaken = 0;
     }
 
+    /**
+     * Returns the amount of sick days the employee has used
+     *
+     * @return The amount of sick days used
+     */
     public double getSickDays()
     {
         return sickDaysTaken;
     }
 
+    /**
+     * Prints out a pay stub for the employee containing their employee information, hourly wage, number of hours assigned, sick days taken,
+     * and the current pay for the month
+     */
     public void printPayStub()
     {
         System.out.println("\n--------------- PAY STUB ---------------");
@@ -56,6 +99,11 @@ public class PartTimeEmployee extends Employee
         System.out.println("----------------------------------------\n");
     }
 
+    /**
+     * Returns string representation of the object
+     *
+     * @return The employee's details
+     */
     @Override
     public String toString()
     {
