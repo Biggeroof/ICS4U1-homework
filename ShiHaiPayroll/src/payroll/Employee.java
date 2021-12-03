dac9720b83ef5aa2687eb5b7f8a83ee1acd5eb68
Hai Shi <58992834+Biggeroof@users.noreply.github.com>
2021-12-03 13:43:06 -0500

    Update Employee.java

--- ShiHaiPayroll/src/payroll/Employee.java
+++ ShiHaiPayroll/src/payroll/Employee.java
@@ -7,6 +7,14 @@ public abstract class Employee
     protected String firstName;
     protected String jobTitle;
 
+    /**
+     * A constructor for the class that is for use in the subclasses
+     *
+     * @param employeeNumber The employee's id
+     * @param lastName The employee's last name
+     * @param firstName The employee's first name
+     * @param jobTitle The employee's job
+     */
     public Employee(String employeeNumber, String lastName, String firstName, String jobTitle)
     {
         this.employeeNumber = employeeNumber;
@@ -15,31 +23,57 @@ public abstract class Employee
         this.jobTitle = jobTitle;
     }
 
+    /**
+     * Returns the id of the employee
+     *
+     * @return The id
+     */
     public String getEmployeeNumber()
     {
         return employeeNumber;
     }
 
+    /**
+     * Returns the last name of the employee
+     *
+     * @return The last name
+     */
     public String getLastName()
     {
         return lastName;
     }
 
+    /**
+     * Returns the first name of the employee
+     *
+     * @return The first name
+     */
     public String getFirstName()
     {
         return firstName;
     }
 
+    /**
+     * Returns the job of the employee
+     *
+     * @return The job
+     */
     public String getJobTitle()
     {
         return jobTitle;
     }
 
+    /**
+     * Returns string representation of the object
+     *
+     * @return The employee's details
+     */
     @Override
     public String toString()
     {
         return "Employee: " + employeeNumber + ", " + firstName + " " + lastName + ", " + jobTitle;
     }
+
 
     abstract double pay();
 
